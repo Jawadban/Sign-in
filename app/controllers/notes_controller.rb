1f325f407e3e1dba8b94d23f426de1a84c1c9812
Jawad Bangash <Jawadban@hotmail.com>
2015-07-13 12:46:16 -0400

    change routes based on if user signed in

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,51 +1,45 @@
 class NotesController < ApplicationController
 	before_action :find_note, only: [:show, :edit, :update, :destroy]
-
+	before_action :authenticate_user!
 
 	def index
-		@notes = Note.all.order("created_at DEC")
-    end
-
-
-    def show
+		@notes = Note.where(user_id: current_user)
 	end
 
-	def new 
-		@note = Note.new
+	def show
+	end
+
+	def new
+		@note = current_user.notes.build
 	end
 
 	def create
-		@note = Note.new(note_params)
+		@note = current_user.notes.build(note_params)
 
-
-	if @note.save
-		redirect_to @note
-	else
-		render 'new'
-	end
+		if @note.save
+			redirect_to @note
+		else
+			render 'new'
+		end
 	end
 
 	def edit
 	end
 
-
 	def update
-		if @note.update(note.params)
-	redirect_to @note
-
-else 
-	render 'edit'
-end
-
+		if @note.update(note_params)
+			redirect_to @note
+		else
+			render 'edit'
+		end
 	end
-
 
 	def destroy
 		@note.destroy
 		redirect_to notes_path
 	end
 
-	private 
+	private
 
 	def find_note
 		@note = Note.find(params[:id])
@@ -53,9 +47,5 @@ end
 
 	def note_params
 		params.require(:note).permit(:title, :content)
-
 	end
-
-
-
 end
